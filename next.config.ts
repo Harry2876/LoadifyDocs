2b99f3cff548c324cb49307a0a2adc535d24894a
ʜᴀʀɪᴏᴍ ʜᴀʀꜱʜ <acbcharryxyz@gmail.com>
2025-01-06 02:35:03 +0530

    Add visuals

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "img.freepik.com/**",
+  //     },
+  //   ],
+  // },
+
   images: {
-    remotePatterns: [
+    remotePatterns: [ 
       {
         protocol: "https",
-        hostname: "img.freepik.com/**",
+        hostname: "github.com/**",
       },
     ],
   },
